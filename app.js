5805f24db0ff17d37804f1e19e067f302828445b
Gabriel Deyo <gabedeyo@gmail.com>
2017-08-07 15:29:04 -0500

    App fixes?

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ var finalhandler = require('finalhandler');
 var http = require('http');
 var serveStatic = require('serve-static');
 
-var serve = serveStatic('./builds/production', {'index': ['index.html', 'index.htm']})
+var serve = serveStatic('./builds/production/', {'index': ['index.html', 'index.htm']})
 
 var server = http.createServer(function onRequest (req, res) {
     serve(req, res, finalhandler(req, res));
 });
 
-server.listen(8080);
+server.listen(8080, function() {
+    console.log('Server listening on localhost8080')
+});
